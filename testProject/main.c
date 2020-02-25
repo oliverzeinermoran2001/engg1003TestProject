da3bdd2f70fd44a4fe5aca39e7f0f41646ab7f1a
Oliver Zeiner Moran <c3355220@uon.edu.au>
2020-02-25 14:35:55 +1100

    Initial Commit

---
+++ testProject/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float x;
+   float y;
+   printf("enter a number: ");
+   scanf("%f", &x);
+   y = 2*x + 15 ;//uncomment this line and write your answer instead
+   printf("y: %f/n", y);
+   return 0;
+}
